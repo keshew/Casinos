d66ab4fff7e6cb7142e5c92df89f8839549be7d2
keshew <artyom.korotckov2017@yandex.ru>
2025-09-22 17:42:18 +0500

    Initial Commit

---
+++ Casinos/CasinosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasinosApp.swift
+//  Casinos
+//
+//  Created by Артём Коротков on 22.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasinosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
